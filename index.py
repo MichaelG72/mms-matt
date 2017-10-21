aee8b21de8ddcf12393a8632fdccbc82b176c2a6
MichaelG72 <garcia.m7234@gmail.com>
2017-10-21 14:28:51 -0500

    testing. 123,123.

--- index.py
+++ index.py
@@ -5,7 +5,8 @@ import datetime
 def handler(event, context):
     data = {
         'output': 'Hello World',
-        'timestamp': datetime.datetime.utcnow().isoformat()
+        'timestamp': datetime.datetime.utcnow().isoformat(),
+        'Test': 'Trying to write. Does it work?'
     }
     return {'statusCode': 200,
             'body': json.dumps(data),
